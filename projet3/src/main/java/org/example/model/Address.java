6e7c719c74fce83f35b54dc95dab217caee63343
baddi youssef <baddiyoussef85@gmail.com>
2022-10-19 15:41:47 +0100

    add projet 3

---
+++ projet3/src/main/java/org/example/model/Address.java
@@ -0,0 +1,17 @@
+
+
+public class Address{
+    private String rue;
+    private String CodeP;
+    private String ville;
+
+    public Address(String rue, String codeP, String ville) {
+        this.rue = rue;
+        CodeP = codeP;
+        this.ville = ville;
+    }
+
+    public void afficher(){
+        System.out.println(id + " " + nom);
+    }
+}
--- projet3/src/main/java/org/example/model/Person.java
+++ projet3/src/main/java/org/example/model/Person.java
@@ -3,10 +3,15 @@
 public class Person{
     private int id;
     private String nom;
+    private Address address;
 
-    public Person(int id, String nom) {
+    private List<String> loisirs;
+
+    public Person(int id, String nom, Address address, List<String> loisirs) {
         this.id = id;
         this.nom = nom;
+        this.address = address;
+        this.loisirs = loisirs;
     }
 
     public void afficher(){
